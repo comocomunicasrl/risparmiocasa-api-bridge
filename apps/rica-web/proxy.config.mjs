f462e2f529e23cad3041e4323a44118aff7460e5
Fernando Walter Gagni <nandowalter@gmail.com>
2025-01-18 11:40:10 +0100

    wip

--- apps/rica-web/proxy.config.mjs
+++ apps/rica-web/proxy.config.mjs
@@ -5,5 +5,13 @@ export default [
       ],
       target: 'http://localhost:3000',
       secure: false
-    }
+    },
+    {
+        context: [
+          '/XA44OBLG'
+        ],
+        target: 'http://localhost:4200',
+        pathRewrite: { '^/XA44OBLG': '' },
+        secure: false
+      }
 ];
