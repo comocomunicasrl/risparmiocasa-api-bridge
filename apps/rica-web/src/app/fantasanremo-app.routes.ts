7920cc2b2d20fa6c60f265a7bf8cd9f0a493f4e9
Fernando Walter Gagni <nandowalter@gmail.com>
2025-01-18 11:19:07 +0100

    Fixed bug in fantasanremo page route.

--- apps/rica-web/src/app/fantasanremo-app.routes.ts
+++ apps/rica-web/src/app/fantasanremo-app.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-    { path: 'XA44OBLG', loadComponent: () => import('./fantasanremo/fantasanremo-page.component').then(m => m.FantasanremoPageComponent) },
+    { path: '', loadComponent: () => import('./fantasanremo/fantasanremo-page.component').then(m => m.FantasanremoPageComponent) },
     { path: 'XA44OBLG', loadComponent: () => import('./fantasanremo/fantasanremo-page.component').then(m => m.FantasanremoPageComponent) }
 ];
