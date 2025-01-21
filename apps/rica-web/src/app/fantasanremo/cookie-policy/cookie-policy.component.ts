faaf8b6d670dcfc5da4832b03e37cc2e5ea51305
Fernando Walter Gagni <nandowalter@gmail.com>
2025-01-21 22:48:20 +0100

    Minor fixes.

--- apps/rica-web/src/app/fantasanremo/cookie-policy/cookie-policy.component.ts
+++ apps/rica-web/src/app/fantasanremo/cookie-policy/cookie-policy.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Inject, ViewChild } from '@angular/core';
 
 @Component({
     standalone: true,
@@ -8,16 +8,13 @@ import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Inject, 
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class CookiePolicyComponent implements OnInit, AfterViewInit {
+export class CookiePolicyComponent implements AfterViewInit {
     @ViewChild('CookieDeclaration') cookieDeclarationEl: ElementRef;
     constructor(
         @Inject(DOCUMENT) private document: Document
     ) { }
 
-    ngOnInit() { }
-
     ngAfterViewInit(): void {
-        // <script id="CookieDeclaration" src="https://consent.cookiebot.com/9c123420-fbfe-4f65-9892-96b11e7efe81/cd.js" type="text/javascript" async>
         const script = this.document.createElement('script');
         script.id = 'CookieDeclaration';
         script.src = 'https://consent.cookiebot.com/9c123420-fbfe-4f65-9892-96b11e7efe81/cd.js';
