4ef0205f92908410e977b7492390891b36c2897c
Fernando Walter Gagni <nandowalter@gmail.com>
2025-01-22 18:50:14 +0100

    Minor fixes.

--- apps/rica-web/proxy.config.mjs
+++ apps/rica-web/proxy.config.mjs
@@ -5,13 +5,5 @@ export default [
       ],
       target: 'http://localhost:3000',
       secure: false
-    },
-    {
-        context: [
-          '/XA44OBLG'
-        ],
-        target: 'http://localhost:4200',
-        pathRewrite: { '^/XA44OBLG': '' },
-        secure: false
-      }
+    }
 ];
