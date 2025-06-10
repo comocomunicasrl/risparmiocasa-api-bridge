b0116e225e6877dc6b0945ec786699a9830b3d94
Fernando Walter Gagni <nandowalter@gmail.com>
2025-06-10 21:33:02 +0200

    Fixed minor bug.

--- apps/cartafedelta/src/pages/api/barcode/[id].tsx
+++ apps/cartafedelta/src/pages/api/barcode/[id].tsx
@@ -2,5 +2,5 @@ import {generateBarcode} from "../../../utils/utils";
 
 export default async (req, res) => {
     res.setHeader("content-disposition", "attachment; filename=barcode.svg");
-    return res.status(200).json(generateBarcode(req.query.id));
+    return res.status(200).send(generateBarcode(req.query.id));
 }
